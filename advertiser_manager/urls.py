697f12b02ee20561912fed30c6f53987970549aa
Amin_Seifi <aminseiifi@gmail.com>
2021-12-27 18:52:23 +0330

    Add path create, created

--- advertiser_manager/urls.py
+++ advertiser_manager/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'advertiser_manager'
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('<int:question_id>/', views.detail, name='detail'),
-    # path('<int:question_id>/results/', views.results, name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote')
+    path('create/', views.create, name='create'),
+    path(r'created/', views.created, name='created'),
 ]

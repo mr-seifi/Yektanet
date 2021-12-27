6351ad7ccf5eeed5ce8413caaa1c76f8b862f5b7
Amin_Seifi <aminseiifi@gmail.com>
2021-12-27 16:12:38 +0330

    Create two Advertiser and Ad models

--- advertiser_manager/models.py
+++ advertiser_manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertiser(models.Model):
+    name = models.CharField(max_length=200)
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    reg_date = models.DateTimeField('date registered')
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=200)
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    img_url = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')

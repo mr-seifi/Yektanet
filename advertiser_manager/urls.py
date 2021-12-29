60f5b7d8178178d0ffbb61e6ddf01441e7b71f72
Amin_Seifi <aminseiifi@gmail.com>
2021-12-29 12:28:41 +0330

    Update url patterns for clicks

--- advertiser_manager/urls.py
+++ advertiser_manager/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 app_name = 'advertiser_manager'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
-    path(r'created/', views.created, name='created'),
+    path('created/', views.created, name='created'),
+    path('click/<int:ad_id>', views.click, name='click')
 ]

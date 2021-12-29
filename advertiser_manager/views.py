ea6f4123d0664a21a1613e34bf841245a979f297
Amin_Seifi <aminseiifi@gmail.com>
2021-12-29 18:28:49 +0330

    Add increaseView static method to IndexView - Some better implementation of django methods

--- advertiser_manager/views.py
+++ advertiser_manager/views.py
@@ -9,12 +9,19 @@ class IndexView(ListView):
     model = Advertiser
     template_name = 'advertiser_manager/index.html'
     context_object_name = 'advertisers'
-    for adv in Advertiser.objects.all():
-        for ad in adv.ad_set.all():
-            ad.views += 1
-            ad.advertiser.views += 1
-            ad.save()
-            ad.advertiser.save()
+
+    def get(self, request, *args, **kwargs):
+        IndexView.increaseViews()
+        return super(IndexView, self).get(request, *args, **kwargs)
+
+    @staticmethod
+    def increaseViews():
+        for adv in Advertiser.objects.all():
+            for ad in adv.ad_set.all():
+                ad.views += 1
+                ad.advertiser.views += 1
+                ad.save()
+                ad.advertiser.save()
 
 
 class CreateView(TemplateView):
@@ -22,6 +29,9 @@ class CreateView(TemplateView):
 
 
 class CreatedView(RedirectView):
+    permanent = False
+    query_string = True
+    pattern_name = 'advertiser_manager:index'
 
     def post(self, request, *args, **kwargs):
         ad = Ad()
@@ -31,13 +41,22 @@ class CreatedView(RedirectView):
         ad.advertiser = get_object_or_404(Advertiser, pk=int(request.POST.get('adv_id', False)))
         ad.pub_date = request.POST.get('date', False)
         ad.save()
-        return HttpResponseRedirect(reverse('advertiser_manager:index', args=()))
+        return super(CreatedView, self).post(request, *args, **kwargs)
 
 
 class ClickRedirectView(RedirectView):
     permanent = False
     query_string = True
-    pattern_name = 'click'
+    pattern_name = 'advertiser_manager:click'
+
+    def get_redirect_url(self, *args, **kwargs):
+        """
+        Return the URL redirect to. Keyword arguments from the URL pattern
+        match generating the redirect request are provided as kwargs to this
+        method.
+        """
+
+        return get_object_or_404(Ad, pk=kwargs['ad_id']).link
 
     def get(self, request, *args, **kwargs):
         ad = get_object_or_404(Ad, pk=kwargs['ad_id'])
@@ -45,5 +64,7 @@ class ClickRedirectView(RedirectView):
         ad.advertiser.clicks += 1
         ad.advertiser.save()
         ad.save()
-        ClickRedirectView.url = ad.link
-        return HttpResponseRedirect('https://google.com')
+        return super(ClickRedirectView, self).get(request, *args, **kwargs)
+
+
+

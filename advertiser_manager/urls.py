a32a0540a3f86ae1cb4db40d669f6b35b0c532b7
Amin_Seifi <aminseiifi@gmail.com>
2021-12-29 16:31:57 +0330

    Change views argument to class-based views argument

--- advertiser_manager/urls.py
+++ advertiser_manager/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic import RedirectView
 from . import views
+from .views import IndexView, CreateView, CreatedView, ClickRedirectView
 
 app_name = 'advertiser_manager'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/', views.create, name='create'),
-    path('created/', views.created, name='created'),
-    path('click/<int:ad_id>', views.click, name='click')
+    path('', IndexView.as_view(), name='index'),
+    path('create/', CreateView.as_view(), name='create'),
+    path('created/', CreatedView.as_view(), name='created'),
+    path('click/<int:ad_id>', ClickRedirectView.as_view(), name='click'),
 ]

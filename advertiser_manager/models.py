ef66082a12f96f374677cb0aade87b776fff863b
Amin_Seifi <aminseiifi@gmail.com>
2021-12-31 18:49:43 +0330

    Add View, Click models - Add approve feature to Ad model - Remove clicks, views features from Advertiser, Ad

--- advertiser_manager/models.py
+++ advertiser_manager/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class Advertiser(models.Model):
     name = models.CharField(max_length=200)
-    clicks = models.IntegerField(default=0)
-    views = models.IntegerField(default=0)
     reg_date = models.DateTimeField('date registered')
 
     def __str__(self):
@@ -13,12 +11,23 @@ class Advertiser(models.Model):
 
 class Ad(models.Model):
     title = models.CharField(max_length=200)
-    clicks = models.IntegerField(default=0)
-    views = models.IntegerField(default=0)
     img_url = models.CharField(max_length=200)
     link = models.CharField(max_length=200)
     advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('date published')
+    approve = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+
+class View(models.Model):
+    ad_id = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    viewed_time = models.DateTimeField('date viewed')
+    user_ip = models.CharField(max_length=30)
+
+
+class Click(models.Model):
+    ad_id = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    clicked_time = models.DateTimeField('date clicked')
+    user_ip = models.CharField(max_length=30)

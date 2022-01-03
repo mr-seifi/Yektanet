df46c6f5e29808d2e442b7924ffcbdea2de59f7d
Amin_Seifi <aminseiifi@gmail.com>
2022-01-03 14:51:39 +0330

    Some changes in url-pattern of click

--- advertiser_manager/urls.py
+++ advertiser_manager/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic import RedirectView
 from . import views
-from .views import IndexView, CreateView, CreatedView, ClickRedirectView
+from .views import IndexView, CreateView, CreatedView, ClickRedirectView, StatView
 
 app_name = 'advertiser_manager'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('create/', CreateView.as_view(), name='create'),
     path('created/', CreatedView.as_view(), name='created'),
-    path('click/<int:ad_id>', ClickRedirectView.as_view(), name='click'),
+    path('click/<int:view_id>', ClickRedirectView.as_view(), name='click'),
+    path('stat/', StatView.as_view(), name='stat')
 ]

0e7f0c6ad269ccb643fc64d6ab46ec86d509e8eb
Amin_Seifi <aminseiifi@gmail.com>
2022-01-04 23:59:30 +0330

    Use ViewSet instead of list and detail classes

--- advertiser_manager_api/views.py
+++ advertiser_manager_api/views.py
@@ -1,128 +1,32 @@
 from advertiser_manager.models import Advertiser, Ad, View, Click
 from advertiser_manager_api.serializers import AdvertiserSerializer, AdSerializer, ViewSerializer, ClickSerializer
-from django.shortcuts import get_object_or_404
-from rest_framework import mixins
-from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework import viewsets
 
 
-class AdvertiserList(generics.GenericAPIView,
-                     mixins.ListModelMixin,
-                     mixins.CreateModelMixin):
+class AdvertiserViewSet(viewsets.ModelViewSet):
+
     queryset = Advertiser.objects.all()
     serializer_class = AdvertiserSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    permission_classes = [IsAuthenticated]
 
 
-class AdvertiserDetail(generics.GenericAPIView,
-                       mixins.RetrieveModelMixin,
-                       mixins.UpdateModelMixin,
-                       mixins.DestroyModelMixin):
-    queryset = Advertiser.objects.all()
-    serializer_class = AdvertiserSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+class AdViewSet(viewsets.ModelViewSet):
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-class AdList(generics.GenericAPIView,
-             mixins.ListModelMixin,
-             mixins.CreateModelMixin):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    permissions = [IsAuthenticatedOrReadOnly]
 
 
-class AdDetail(generics.GenericAPIView,
-               mixins.RetrieveModelMixin,
-               mixins.UpdateModelMixin,
-               mixins.DestroyModelMixin):
-    queryset = Ad.objects.all()
-    serializer_class = AdSerializer
+class ViewViewSet(viewsets.ModelViewSet):
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-class ViewList(generics.GenericAPIView,
-               mixins.ListModelMixin,
-               mixins.CreateModelMixin):
     queryset = View.objects.all()
     serializer_class = ViewSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-class ViewDetail(generics.GenericAPIView,
-                 mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin):
-    queryset = View.objects.all()
-    serializer_class = ViewSerializer
+class ClickViewSet(viewsets.ModelViewSet):
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-class ClickList(generics.GenericAPIView,
-                mixins.ListModelMixin,
-                mixins.CreateModelMixin):
     queryset = Click.objects.all()
     serializer_class = ClickSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ClickDetail(generics.GenericAPIView,
-                  mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.DestroyModelMixin):
-    queryset = Click.objects.all()
-    serializer_class = ClickSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    permission_classes = [IsAuthenticatedOrReadOnly]

e0e0c4ad78152c16cd37dba8536a05c11542c84f
Amin_Seifi <aminseiifi@gmail.com>
2022-01-04 19:18:59 +0330

    Add format_suffix_patterns - Add app_name

--- advertiser_manager_api/urls.py
+++ advertiser_manager_api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
+app_name = 'advertiser_manager_api'
 urlpatterns = [
     path('advertisers/',),
     path('ads/',),
     path('views/',),
     path('clicks/',),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)

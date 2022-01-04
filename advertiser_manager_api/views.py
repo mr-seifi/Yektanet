bae20de1f921b57c4d79dbf896344a65f5d7a09a
Amin_Seifi <aminseiifi@gmail.com>
2022-01-04 19:32:45 +0330

    Add Model ApiViews

--- advertiser_manager_api/views.py
+++ advertiser_manager_api/views.py
@@ -1,3 +1,125 @@
-from django.shortcuts import render
+from advertiser_manager.models import Advertiser, Ad, View, Click
+from advertiser_manager_api.serializers import AdvertiserSerializer, AdSerializer, ViewSerializer, ClickSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import mixins
+from rest_framework import generics
 
-# Create your views here.
+
+class AdvertiserList(generics.GenericAPIView,
+                     mixins.ListModelMixin,
+                     mixins.CreateModelMixin):
+    queryset = Advertiser.objects.all()
+    serializer_class = AdvertiserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class AdvertiserDetail(generics.GenericAPIView,
+                       mixins.RetrieveModelMixin,
+                       mixins.UpdateModelMixin,
+                       mixins.DestroyModelMixin):
+    queryset = Advertiser.objects.all()
+    serializer_class = AdvertiserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class AdList(generics.GenericAPIView,
+             mixins.ListModelMixin,
+             mixins.CreateModelMixin):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class AdDetail(generics.GenericAPIView,
+               mixins.RetrieveModelMixin,
+               mixins.UpdateModelMixin,
+               mixins.DestroyModelMixin):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class ViewList(generics.GenericAPIView,
+               mixins.ListModelMixin,
+               mixins.CreateModelMixin):
+    queryset = View.objects.all()
+    serializer_class = ViewSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class ViewDetail(generics.GenericAPIView,
+                 mixins.RetrieveModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin):
+    queryset = View.objects.all()
+    serializer_class = ViewSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class ClickList(generics.GenericAPIView,
+                mixins.ListModelMixin,
+                mixins.CreateModelMixin):
+    queryset = Click.objects.all()
+    serializer_class = ClickSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class ClickDetail(generics.GenericAPIView,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin):
+    queryset = Click.objects.all()
+    serializer_class = ClickSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)

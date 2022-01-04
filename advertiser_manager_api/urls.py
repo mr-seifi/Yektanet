04722c3e765b11c108ee03e113f4c47e77f3cb20
Amin_Seifi <aminseiifi@gmail.com>
2022-01-04 19:32:54 +0330

    Add Api urlpatterns

--- advertiser_manager_api/urls.py
+++ advertiser_manager_api/urls.py
@@ -4,10 +4,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'advertiser_manager_api'
 urlpatterns = [
-    path('advertisers/',),
-    path('ads/',),
-    path('views/',),
-    path('clicks/',),
+    path('advertisers/', views.AdvertiserList.as_view()),
+    path('advertisers/<int:pk>/', views.AdvertiserDetail.as_view()),
+    path('ads/', views.AdList.as_view()),
+    path('ads/<int:pk>/', views.AdDetail.as_view()),
+    path('views/', views.ViewList.as_view()),
+    path('views/<int:pk>/', views.ViewDetail.as_view()),
+    path('clicks/', views.ClickList.as_view()),
+    path('clicks/<int:pk>/', views.ClickDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

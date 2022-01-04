1e6e80f7aae5895c47188684d076b03eab46ef66
Amin_Seifi <aminseiifi@gmail.com>
2022-01-04 19:10:26 +0330

    Create Serializers for models

---
+++ advertiser_manager/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Advertiser, Ad, View, Click
+
+
+class AdvertiserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Advertiser
+        fields = ['id', 'name', 'reg_date']
+
+
+class AdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ad
+        fields = ['id', 'title', 'img_url', 'link', 'advertiser', 'pub_date', 'approve']
+
+
+class ViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = View
+        fields = ['id', 'ad', 'viewed_time', 'user_ip']
+
+
+class ClickSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Click
+        fields = ['id', 'view', 'clicked_time', 'user_ip']

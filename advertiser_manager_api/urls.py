56fd88624ba3614d440e997ecc3f856aa69143d0
Amin_Seifi <aminseiifi@gmail.com>
2022-01-04 23:58:36 +0330

    Delete all paths and use router instead of that

--- advertiser_manager_api/urls.py
+++ advertiser_manager_api/urls.py
@@ -1,17 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
 app_name = 'advertiser_manager_api'
-urlpatterns = [
-    path('advertisers/', views.AdvertiserList.as_view()),
-    path('advertisers/<int:pk>/', views.AdvertiserDetail.as_view()),
-    path('ads/', views.AdList.as_view()),
-    path('ads/<int:pk>/', views.AdDetail.as_view()),
-    path('views/', views.ViewList.as_view()),
-    path('views/<int:pk>/', views.ViewDetail.as_view()),
-    path('clicks/', views.ClickList.as_view()),
-    path('clicks/<int:pk>/', views.ClickDetail.as_view()),
-]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+router = DefaultRouter()
+router.register(r'advertisers', views.AdvertiserViewSet)
+router.register(r'ads', views.AdViewSet)
+router.register(r'views', views.ViewViewSet)
+router.register(r'clicks', views.ClickViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]

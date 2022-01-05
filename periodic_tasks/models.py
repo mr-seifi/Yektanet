fe629c2c984a11b43fa21f5bdb1e63cfc0d1c305
Amin_Seifi <aminseiifi@gmail.com>
2022-01-06 01:20:56 +0330

    Create some models for save Clicked, Viewed datas

--- periodic_tasks/models.py
+++ periodic_tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ClickedCount(models.Model):
+
+    date = models.DateTimeField()
+    count = models.IntegerField()
+
+
+class ViewedCount(models.Model):
+
+    date = models.DateTimeField()
+    count = models.IntegerField()
